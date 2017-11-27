5090b9584d0cb4482f06f4be251e691c2fab5300
lfvm2410 <luis_f_94@hotmail.com>
2017-11-26 22:26:06 -0600

    Sección #4. Clase 6 y 7. Creando mi primer proyecto MVC
    Se creo un proyecto mvc, donde se configuro un layout general para el proyecto, así como la integración de bootstrap y jquery al proyecto

---
+++ Sección #3/Clase #6/MiPrimerProyectoMVC/MiPrimerProyectoMVC/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiPrimerProyectoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //Action Result permite retornar vistas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Alumno()
+        {
+            return View();
+        }
+    }
+}
