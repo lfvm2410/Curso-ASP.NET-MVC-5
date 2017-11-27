363d602691678d72ac0cb3b9aa9581812007b4e9
lfvm2410 <luis_f_94@hotmail.com>
2017-11-27 05:41:55 -0600

    Sección #4. Clase 8. Parámetros MVC
    Enviando parámetros a la vista desde el controlador y viceversa

--- Sección #3/Clase #6/MiPrimerProyectoMVC/MiPrimerProyectoMVC/Controllers/HomeController.cs
+++ Sección #3/Clase #6/MiPrimerProyectoMVC/MiPrimerProyectoMVC/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Model;
 
 namespace MiPrimerProyectoMVC.Controllers
 {
@@ -12,12 +13,28 @@ namespace MiPrimerProyectoMVC.Controllers
         //Action Result permite retornar vistas
         public ActionResult Index()
         {
-            return View();
+            //Viewbag se utiliza para setear datos que se pueden recoger en la vista
+            ViewBag.Algo = "Soy algo";
+            ViewBag.Alumnos = Alumno.Listar();
+            ViewBag.Alumno = Alumno.Obtener();
+
+            //Se le pasa modelo a la vista para listar los alumnos
+            //Mediante el view se especifica el modelo principal de la vista
+            //Con ViewBag se pasan las demas cosas
+            return View(Alumno.Listar());
         }
 
-        public ActionResult Alumno()
+        public ActionResult Ver(int id = 0)
         {
-            return View();
+            ViewBag.id = id;
+
+            return View(Alumno.Obtener());
+        }
+
+        public ActionResult Guardar(Alumno alumno, int algo, int[] pruebaArray)
+        {
+            //Redireccionar a otro metodo del controlador
+            return Redirect("~/Home/Index");
         }
     }
 }
---
+++ Sección #3/Clase #6/MiPrimerProyectoMVC/Model/Alumno.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public static List<Alumno> Listar()
+        {
+            var alumnos = new List<Alumno>();
+
+            for (var i = 0; i <= 10; i++)
+            {
+                alumnos.Add(new Alumno()
+                {
+                    Id = i,
+                    Nombre = "Alumno " + i
+                });
+            }
+
+            return alumnos;
+
+        }
+
+        public static Alumno Obtener()
+        {
+            return new Alumno
+            {
+                Id = 1,
+                Nombre = "Alumno 1"
+            };
+        }
+
+    }
+}
